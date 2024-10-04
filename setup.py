2494b6770e697d8e8e1d2ee7a14f7bf2e3349101
Ramon Braga <140903176+ramon-braga@users.noreply.github.com>
2024-10-04 20:56:56 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+    with open("requirements.txt") as f:
+        requirements = f.read().splitlines()
+
+    setup(
+        name="image-processing-package",
+        version="0.0.1",
+        author="Ramon",
+        author_email="my_email",
+        description="my short description",
+        long_description=page_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/ramon-braga/image-processing-package",
+        packages=find_packages(),
+        install_requires=requirements,
+        python_requires='>=3.8'
+    )
